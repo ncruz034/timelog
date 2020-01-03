43d136c64a060731d648403b4f6e86e7022f12fe
Nelson <cruz_nel27@yahoo.com>
2020-01-03 13:46:44 -0500

    preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,13 @@ const express = require('express');
 const app = express();
 
 //Remove cors in production app
-app.use(cors());
+//app.use(cors());
 app.use(express.static(__dirname + '/dist'));
 require('./startup/routes')(app);
 require('./startup/logging');
 require('./startup/databaseInit')();
 require('./startup/config')();
+require('./startup/prod')(app);
 //require('./startup/validation')();
 
 const port = process.env.PORT || 8080;
