030ee07e14ff8bb7e266afa379aebcaf931ff0da
Nelson <cruz_nel27@yahoo.com>
2020-01-04 10:38:15 -0500

    Fixed mongoose wornings for deployment

--- backend/startup/databaseInit.js
+++ backend/startup/databaseInit.js
@@ -5,7 +5,7 @@ const winston = require('winston');
 
 module.exports = function(){
     //mongoose.connect('mongodb://localhost:27017/shiskindb',{useNewUrlParser:true})
-    mongoose.connect('mongodb://ncruz:Piloto27@ds141932.mlab.com:41932/shiskindb',{useNewUrlParser:true})
+    mongoose.connect('mongodb://ncruz:Piloto27@ds141932.mlab.com:41932/shiskindb',{useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true})
     .then(()=> winston.info('Connected to MongoDB...'))
 
 }
