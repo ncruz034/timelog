639d22a1f3e1333f3e474a363e691ab953b993a9
Nelson <cruz_nel27@yahoo.com>
2020-01-06 06:48:17 -0500

    Fixing access control

--- server.js
+++ server.js
@@ -5,7 +5,22 @@ const express = require('express');
 const app = express();
 //Remove cors in production app
 app.use(cors());
-app.use(express.static(__dirname + '/dist/'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + '/dist/'));
+}
+
 require('./startup/routes')(app);
 require('./startup/logging');
 require('./startup/databaseInit')();
