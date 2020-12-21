15c6d31731f4f2206784eeed6bacdd667d8138fd
Nelson <cruz_nel27@yahoo.com>
2020-12-21 12:52:30 -0500

    change database to mongodb Atlas

--- startup/databaseInit.js
+++ startup/databaseInit.js
@@ -5,7 +5,10 @@ const winston = require('winston');
 
 module.exports = function(){
     //mongoose.connect('mongodb://localhost:27017/shiskindb',{useNewUrlParser:true})
-    mongoose.connect('mongodb://ncruz:Piloto27@ds141932.mlab.com:41932/shiskindb',{useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true})
+    //Mongodb Atlas connection:
+    //mongodb+srv://ncruz:<password>@cluster0.vwmn6.mongodb.net/shiskindb?retryWrites=true&w=majority
+    mongoose.connect('mongodb+srv://ncruz:Piloto27@cluster0.vwmn6.mongodb.net/shiskindb?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true})
     .then(()=> winston.info('Connected to MongoDB...'))
-
+    //.connect('mongodb://ncruz:Piloto27@ds141932.mlab.com:41932/shiskindb',{useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true})
+    //.then(()=> winston.info('Connected to MongoDB...'))
 }
