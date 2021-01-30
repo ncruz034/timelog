78dbe7a58e035922ce114959796aee2c1f9e5d92
Nelson <cruz_nel27@yahoo.com>
2021-01-30 17:27:01 -0500

    temporary redirection of backend for deployment purpose

--- startup/databaseInit.js
+++ startup/databaseInit.js
@@ -7,8 +7,14 @@ module.exports = function(){
     //mongoose.connect('mongodb://localhost:27017/shiskindb',{useNewUrlParser:true})
     //Mongodb Atlas connection:
     //mongodb+srv://ncruz:<password>@cluster0.vwmn6.mongodb.net/shiskindb?retryWrites=true&w=majority
+
+
+
     mongoose.connect('mongodb+srv://ncruz:Piloto27@cluster0.vwmn6.mongodb.net/shiskindb?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true})
-    .then(()=> winston.info('Connected to MongoDB...'))
+    .then(()=> winston.info('Connected to MongoDB ATLAS...'))
+
+
+
     //.connect('mongodb://ncruz:Piloto27@ds141932.mlab.com:41932/shiskindb',{useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true})
     //.then(()=> winston.info('Connected to MongoDB...'))
 }
