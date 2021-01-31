f8b592f344bb6b96feca2d01136272384993d493
Nelson <cruz_nel27@yahoo.com>
2021-01-31 16:06:19 -0500

    redirect backend

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,3 +1,3 @@
- export const theUri: string = 'http://localhost:3000/api';
+ //export const theUri: string = 'http://localhost:3000/api';
 
-//export const theUri: string = 'https://ssa-timelog.herokuapp.com/api';
+export const theUri: string = 'https://ssa-timelog.herokuapp.com/api';
