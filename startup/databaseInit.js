436eab917b902b4ee5e7102a687f8381d1fc1bc1
Nelson Cruz <cruz_nel27@yahoo.com>
2021-02-03 14:28:49 -0500

    Update databaseInit.js

--- startup/databaseInit.js
+++ startup/databaseInit.js
@@ -6,15 +6,15 @@ const winston = require('winston');
 module.exports = function(){
     //mongoose.connect('mongodb://localhost:27017/shiskindb',{useNewUrlParser:true})
     //Mongodb Atlas connection:
-    //mongodb+srv://ncruz:<password>@cluster0.vwmn6.mongodb.net/shiskindb?retryWrites=true&w=majority
+    //mongodb+srv://<username:password>@cluster0.vwmn6.mongodb.net/shiskindb?retryWrites=true&w=majority
 
 
 
-    mongoose.connect('mongodb+srv://ncruz:Piloto27@cluster0.vwmn6.mongodb.net/shiskindb?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true})
+    mongoose.connect('mongodb+srv://<username:password>@cluster0.vwmn6.mongodb.net/shiskindb?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true})
     .then(()=> winston.info('Connected to MongoDB ATLAS...'))
 
 
 
-    //.connect('mongodb://ncruz:Piloto27@ds141932.mlab.com:41932/shiskindb',{useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true})
+    //.connect('mongodb://<username:password>@ds141932.mlab.com:41932/shiskindb',{useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true})
     //.then(()=> winston.info('Connected to MongoDB...'))
 }
